acb62972762d2ef6fe757c49da9d43e5b947dc18
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2020-12-04 12:07:46 -0300

    initial commit

---
+++ ISBNValidator/src/br/com/henricolazuroz/isbntools/ValidateISBN.java
@@ -0,0 +1,4 @@
+package br.com.henricolazuroz.isbntools;
+
+public class ValidateISBN {
+}
---
+++ ISBNValidator/test/br/com/henricolazuroz/isbntools/ExampleTest.java
@@ -0,0 +1,13 @@
+package br.com.henricolazuroz.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+        fail();
+    }
+}
