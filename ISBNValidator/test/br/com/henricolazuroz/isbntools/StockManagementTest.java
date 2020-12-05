f69280b5735eaf5edec3182d77b0862e14b88533
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2020-12-05 12:17:06 -0300

    begin of chapter 7

---
+++ ISBNValidator/test/br/com/henricolazuroz/isbntools/StockManagementTest.java
@@ -0,0 +1,13 @@
+package br.com.henricolazuroz.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StockManagementTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
