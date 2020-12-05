97431ff5f6bdcb7648fcf8e1e87d8ef4a74412b0
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2020-12-05 12:15:21 -0300

    end of chapter 6

--- ISBNValidator/src/br/com/henricolazuroz/isbntools/ValidateISBN.java
+++ ISBNValidator/src/br/com/henricolazuroz/isbntools/ValidateISBN.java
@@ -2,15 +2,49 @@ package br.com.henricolazuroz.isbntools;
 
 public class ValidateISBN {
 
+    public static final int LONG_ISBN = 13;
+    public static final int SHORT_ISBN = 10;
+    public static final int LONG_ISBN_MULTIPLIER = 10;
+    public static final int SHORT_ISBN_MULTIPLIER = 11;
+
     public boolean checkISBN(String isbn) {
+        if (isbn.length() == LONG_ISBN)
+            return isAValidLongISBN(isbn);
+        else if (isbn.length() == SHORT_ISBN)
+            return isAValidShortISBN(isbn);
+
+        throw new NumberFormatException("ISBN numbers must be " + SHORT_ISBN +  " or " + LONG_ISBN+ " digits long!");
+    }
+
+    private boolean isAValidShortISBN(String isbn) {
+        if (isbn.length() != SHORT_ISBN)
+            throw new NumberFormatException("ISBN numbers must be 10 digits long!");
+
         int total = 0;
-        for (int i = 0; i < 10; i++) {
-            total += (int) isbn.charAt(i) * (10 - i);
+        for (int i = 0; i < SHORT_ISBN; i++) {
+            if (!Character.isDigit(isbn.charAt(i))) {
+                if (i == 9 && isbn.charAt(i) == 'X')
+                    total += 10;
+                else
+                    throw new NumberFormatException("ISBN numbers can only contains numeric digits!");
+            } else {
+                total += Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN - i);
+            }
         }
 
-        if (total % 11 == 0)
-            return true;
+        return total % SHORT_ISBN_MULTIPLIER == 0;
+    }
 
-        return false;
+    private boolean isAValidLongISBN(String isbn) {
+        int total = 0;
+
+        for (int i = 0; i < LONG_ISBN; i++) {
+            if (i % 2 == 0)
+                total += Character.getNumericValue(isbn.charAt(i));
+            else
+                total += Character.getNumericValue(isbn.charAt(i)) * 3;
+        }
+
+        return total % LONG_ISBN_MULTIPLIER == 0;
     }
 }
--- ISBNValidator/test/br/com/henricolazuroz/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/br/com/henricolazuroz/isbntools/ValidateISBNTest.java
@@ -2,12 +2,14 @@ package br.com.henricolazuroz.isbntools;
 
 import org.junit.Test;
 
+import java.text.NumberFormat;
+
 import static org.junit.Assert.*;
 
 public class ValidateISBNTest {
 
     @Test
-    public void checkAValidISBN() {
+    public void checkAValid10DigitISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449116");
         assertTrue("first result", result);
@@ -16,9 +18,49 @@ public class ValidateISBNTest {
     }
 
     @Test
-    public void checkAnInvalidISBN() {
+    public void checkAValid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9788560280940");
+        assertTrue("first result", result);
+        result = validator.checkISBN("9788576573135");
+        assertTrue("second result", result);
+    }
+
+    @Test
+    public void checkAnInvalid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9788560280941");
+        assertFalse(result);
+    }
+
+    @Test
+    public void TenDigitISBNNumbersEndingInAnXAreValid() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("012000030X");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkAnInvalid10DigitISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nineDigitISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        validator.checkISBN("123456789");
+
+        /*
+         JUnit 5 ->
+         assertThrows(NumberFormatException.class, () -> {validator.checkISBN("123456789")})
+         */
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nonNumericISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        validator.checkISBN("helloworld");
     }
 }
